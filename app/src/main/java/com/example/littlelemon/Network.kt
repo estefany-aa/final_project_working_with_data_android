42e1ca48a0005515caa5152c46fb475b049fb619
estefany.aa <me.amaya234@gmail.com>
2024-03-03 18:49:48 -0500

    Fix comments and add app's screenshots

--- app/src/main/java/com/example/littlelemon/Network.kt
+++ app/src/main/java/com/example/littlelemon/Network.kt
@@ -4,23 +4,17 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MenuNetwork(
-    // add code here
-    // Add properties to match the structure of the JSON dictionary representing the menu
     val menu: List<MenuItemNetwork>
 )
 
 @Serializable
 data class MenuItemNetwork(
-    //add code here
-    // Add properties to match the structure of the JSON dictionary representing the menu items
     val id: Int,
     val title: String,
     val price: Double
 
 ) {
     fun toMenuItemRoom() = MenuItemRoom(
-        // add code here
-        // Use all of the properties of the MenuItemNetwork
         id,
         title,
         price
